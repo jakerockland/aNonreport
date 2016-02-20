8751c1205ed89025a59fbe12c2a9097982a6075f
Jake Rockland <jakerockland@email.arizona.edu>
2016-02-20 15:44:33 +0100

    test encryption setup

--- aNonreport/app/js/logic.js
+++ aNonreport/app/js/logic.js
@@ -1,23 +1,33 @@
-$(document).on('change', '.btn-file :file', function() {
-  var input = $(this);
-  var files = input.get(0).files;
-  console.log(files.length);
+$(document).on('change', '.btn-file :file', function(e) {
+  var input = e.target;
 
-  var mt = Random.engines.mt19937();
-  mt.autoSeed();
-  var sessionKey = Random.hex(true)(mt, 32);
-  console.log(sessionKey);
+  var files = input.files;
+  console.log(files.length);
 
   for (var i = 0; i < files.length; i++) {
     file = files[i];
     console.log(file);
     var label = file.name;
     console.log(label);
+    var reader = new FileReader();
 
-    var reader = new FileReader(file);
-    reader.onload = function(e) {
-      var encrypted = CryptoJS.AES.encrypt(e.target.result, sessionKey);
+    reader.onload = function() {
+      var dataURL = reader.result;
+
+      console.log(dataURL);
+
+      var mt = Random.engines.mt19937();
+      mt.autoSeed();
+      var sessionKey = Random.hex(true)(mt, 32);
+      console.log(sessionKey);
+
+      var encrypted = CryptoJS.AES.encrypt(dataURL, sessionKey);
       console.log(encrypted.toString());
-    }
-  }
+
+      var decrypted = CryptoJS.AES.decrypt(encrypted, sessionKey);
+      console.log(decrypted.toString(CryptoJS.enc.Latin1));
+    };
+
+    reader.readAsDataURL(file);
+  };
 });
