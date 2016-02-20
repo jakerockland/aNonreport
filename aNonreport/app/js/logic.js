17b0ecfc34ec07cfd2c9b5f7522b48eca727cd0d
Jake Rockland <jakerockland@email.arizona.edu>
2016-02-20 16:54:26 +0100

    finished upload encrpytion relay, added commented out console log statements for debug purposes

--- aNonreport/app/js/logic.js
+++ aNonreport/app/js/logic.js
@@ -2,46 +2,47 @@ $(document).on('change', '.btn-file :file', function(e) {
   var input = e.target;
 
   var files = input.files;
-  console.log(files.length);
+  // console.log('Number of Files:\n' + files.length);
 
   for (var i = 0; i < files.length; i++) {
     file = files[i];
-    console.log(file);
     var label = file.name;
-    console.log(label);
+    // console.log('File Name:\n' + label);
+    // console.log('File Data:\n' + file);
 
     //  session key generation block
     var mt = Random.engines.mt19937();
     mt.autoSeed();
     var sessionKey = Random.hex(true)(mt, 32);
-    console.log(sessionKey);
+    // console.log('Session Key:\n' + sessionKey);
 
     // rsa encryption block
     var rsaKey = cryptico.generateRSAKey('UnitedNations', 1024);
     var publicKey = cryptico.publicKeyString(rsaKey);
-    console.log(publicKey);
+    // console.log('Public RSA Key:\n' + publicKey);
 
     // read in and encrypt file
     var reader = new FileReader();
     reader.onload = function() {
       var dataURL = reader.result;
 
-      console.log(dataURL);
+      // console.log('URL Data:\n' + dataURL);
 
       // AES encrypt file with session key
       var encrypted = CryptoJS.AES.encrypt(dataURL, sessionKey);
-      console.log(encrypted.toString());
+      // console.log('Encrypted Data:\n' + encrypted.toString());
 
       // encrypt session key with RSA
       var encryptedSessionKey = cryptico.encrypt(sessionKey, publicKey).cipher;
-      console.log(encryptedSessionKey);
+      // console.log('Encrypted Session Key:\n' + encryptedSessionKey);
 
       // decrypted session key
       var decryptedSessionKey = cryptico.decrypt(encryptedSessionKey, cryptico.generateRSAKey('UnitedNations', 1024)).plaintext;
+      // console.log('Decrypted Session Key:\n' + decryptedSessionKey);
 
       // decrypt file with decrypted session key
       var decrypted = CryptoJS.AES.decrypt(encrypted, decryptedSessionKey);
-      console.log(decrypted.toString(CryptoJS.enc.Latin1));
+      // console.log('Decrypted File:\n' + decrypted.toString(CryptoJS.enc.Latin1));
     };
 
     reader.readAsDataURL(file);
