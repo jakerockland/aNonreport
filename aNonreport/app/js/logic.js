ac93a4b8fa4c24bf8b81c68e93518ddd868c1416
Jake Rockland <jakerockland@email.arizona.edu>
2016-02-20 15:54:33 +0100

    added cryptico

--- aNonreport/app/js/logic.js
+++ aNonreport/app/js/logic.js
@@ -9,17 +9,17 @@ $(document).on('change', '.btn-file :file', function(e) {
     console.log(file);
     var label = file.name;
     console.log(label);
-    var reader = new FileReader();
 
+    var mt = Random.engines.mt19937();
+    mt.autoSeed();
+    var sessionKey = Random.hex(true)(mt, 32);
+    console.log(sessionKey);
+
+    var reader = new FileReader();
     reader.onload = function() {
       var dataURL = reader.result;
 
       console.log(dataURL);
-
-      var mt = Random.engines.mt19937();
-      mt.autoSeed();
-      var sessionKey = Random.hex(true)(mt, 32);
-      console.log(sessionKey);
 
       var encrypted = CryptoJS.AES.encrypt(dataURL, sessionKey);
       console.log(encrypted.toString());
