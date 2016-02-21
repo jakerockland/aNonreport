5c5196c443d3a414de23dd414f86f4893ca5d0d0
Jake Rockland <jakerockland@email.arizona.edu>
2016-02-21 08:09:38 +0100

    added AWS uploads

--- aNonreport/app/js/decrypt.js
+++ aNonreport/app/js/decrypt.js
@@ -16,31 +16,3 @@ var decrypt = function(encryptedPacket, passphrase) {
 
   return decryptedData;
 };
-
-// var decryptedData = decrypt(encryptedPacket, 'UnitedNations');
-<<<<<<< HEAD
-=======
-
-var uploadData = function() {
-
-  var encryptedPacket = $('#report').data('encryptedPacket');
-  var hashData = $('#report').data('hashData');
-  if (encryptedPacket) {
-    console.log('Encrypted Packet:\n' + encryptedPacket);
-    console.log('Data Hash:\n' + hashData);
-    console.log(Date.now());
-    console.log($('#location').data('latitude'));
-    console.log($('#location').data('longitude'));
-    
-    var categories = [];
-    $('#checkboxes input:checked').each(function() {
-      categories.push($(this).attr('name'));
-    });
-
-    console.log(categories);
-    alert('Thank you, your report has been submitted.');
-  } else {
-    alert('No file uploaded for reporting!');
-  }
-};
->>>>>>> 29c82b3b3754d533bbf21f98460f84df574f289f
--- aNonreport/app/js/index.js
+++ aNonreport/app/js/index.js
@@ -1,20 +1,65 @@
+AWS.config.update({
+  accessKeyId: 'AKIAJEPRFK36DC5KIFIQ',
+  secretAccessKey: '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',
+});
+AWS.config.region = 'eu-central-1';
+
+var s3 = new AWS.S3();
+
 var uploadData = function() {
 
   var encryptedPacket = $('#report').data('encryptedPacket');
   var hashData = $('#report').data('hashData');
   if (encryptedPacket) {
-    console.log('Encrypted Packet:\n' + encryptedPacket);
-    console.log('Data Hash:\n' + hashData);
-    console.log(Date.now());
-    console.log($('#location').data('latitude'));
-    console.log($('#location').data('longitude'));
+
     var categories = [];
     $('#checkboxes input:checked').each(function() {
       categories.push($(this).attr('name'));
     });
 
-    console.log(categories);
-    alert('Thank you, your report has been submitted.');
+    var key = String(Math.floor((Date.now() / 1000)));
+
+    var metaData = JSON.stringify({
+      hash: hashData,
+      latitude: $('#location').data('latitude'),
+      longitude: $('#location').data('longitude'),
+      categories: categories,
+    });
+
+    var fileParams = {
+      Bucket: 'a-non-report',
+      Key: 'data/' + key,
+      Body: encryptedPacket,
+    };
+
+    var ledgerParams = {
+      Bucket: 'a-non-report',
+      Key: 'ledger/' + key,
+      Body: metaData,
+    };
+
+    s3.putObject(fileParams, function(err, data) {
+      if (err) {
+        console.log(err, err.stack); // an error occurred
+        alert('Error uploading encrypted packet.');
+      } else {
+        s3.putObject(ledgerParams, function(err, data) {
+          if (err) {
+            console.log(err, err.stack); // an error occurred
+            alert('Error uploading ledger information.');
+          } else {
+            console.log(data); // successful response
+            alert('Thank you, your report has been submitted.');
+          }
+        });
+
+        console.log(data); // successful response
+      }
+    });
+
+    console.log('Ledger Parameters:\n' + ledgerParams);
+    console.log('Encrypted Packet:\n' + encryptedPacket);
+
   } else {
     alert('No file uploaded for reporting!');
   }
