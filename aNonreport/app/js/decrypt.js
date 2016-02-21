9483949c29ed98d3a24ea73ff4a50622bdca5390
Jake Rockland <jakerockland@email.arizona.edu>
2016-02-21 06:29:07 +0100

    refactored js

--- aNonreport/app/js/decrypt.js
+++ aNonreport/app/js/decrypt.js
@@ -18,25 +18,3 @@ var decrypt = function(encryptedPacket, passphrase) {
 };
 
 // var decryptedData = decrypt(encryptedPacket, 'UnitedNations');
-
-var uploadData = function() {
-
-  var encryptedPacket = $('#report').data('encryptedPacket');
-  var hashData = $('#report').data('hashData');
-  if (encryptedPacket) {
-    console.log('Encrypted Packet:\n' + encryptedPacket);
-    console.log('Data Hash:\n' + hashData);
-    console.log(Date.now());
-    console.log($('#location').data('latitude'));
-    console.log($('#location').data('longitude'));
-    var categories = [];
-    $('#checkboxes input:checked').each(function() {
-      categories.push($(this).attr('name'));
-    });
-
-    console.log(categories);
-    alert('Thank you, your report has been submitted.');
-  } else {
-    alert('No file uploaded for reporting!');
-  }
-};
--- aNonreport/app/js/index.js
+++ aNonreport/app/js/index.js
@@ -1,4 +1,21 @@
-// $(document).ready(function() {
-//   AnonReport.set(10);
-//   var value = AnonReport.get().toNumber();
-// });
+var uploadData = function() {
+
+  var encryptedPacket = $('#report').data('encryptedPacket');
+  var hashData = $('#report').data('hashData');
+  if (encryptedPacket) {
+    console.log('Encrypted Packet:\n' + encryptedPacket);
+    console.log('Data Hash:\n' + hashData);
+    console.log(Date.now());
+    console.log($('#location').data('latitude'));
+    console.log($('#location').data('longitude'));
+    var categories = [];
+    $('#checkboxes input:checked').each(function() {
+      categories.push($(this).attr('name'));
+    });
+
+    console.log(categories);
+    alert('Thank you, your report has been submitted.');
+  } else {
+    alert('No file uploaded for reporting!');
+  }
+};
